280d270aefa6159dbd81a03683d67b6e4015af84
Mohammad Sami Zaman <samizaman987@gmail.com>
2026-09-23 20:30:07 +0600

    sami 4th commit pushing branch called Feature

--- commit_2.py
+++ commit_2.py
@@ -4,3 +4,5 @@ list_a.append(1)
 list_a.append(2)
 
 print(list_a)
+
+print("This is a test for commit 4 at branch called Feature")
---
+++ commit_4_fea.py
@@ -0,0 +1,6 @@
+print("This is a test for commit 4 at branch called Feature")
+
+num1 = 10
+num2 = 20
+
+print("The multiplication of", num1, "and", num2, "is:", num1 * num2)
